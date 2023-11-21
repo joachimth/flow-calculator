b774cfa6f9d0f673f548443270c8fb61290aa493
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2023-11-21 21:18:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-    return defineConfig({
-        build: {
-            outDir: 'build',
-        },
-        plugins: [react()],
-        base: '/flow-calculator/',
-        define: {
-            "process.env.NODE_ENV": `"${mode}"`,
-        }
-    })
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/flow-calculator/',
+  define: {
+      "process.env.NODE_ENV": `"${mode}"`,
+  }
+})
